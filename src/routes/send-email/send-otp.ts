554543121883750c499b6ca3e4e6706a4c459970
Masum Billah <masumbillah6584@gmail.com>
2024-10-21 20:20:28 +0600

    added comments and documents for api routes

--- src/routes/send-email/send-otp.ts
+++ src/routes/send-email/send-otp.ts
@@ -1,15 +1,59 @@
 import { Router, Request, Response } from 'express';
 
-// TODO: need to specify import comment
+// import render from '@react-email/render';
 import { render } from '@react-email/components';
 
-import { OTPTemplate } from '../../template/otp-verification';
-import sendZeptoEmail from '../../utils/email-trigger';
-
-import { sendOtpSchema, sendOtpSchemaType } from '../../validation/schema';
+// validation middleware
 import validate from '../../validation';
 
+// import utils imports for sending emails
+import sendZeptoEmail from '../../utils/email-trigger';
+
+// import schema & types imports for validation
+import { OTPTemplate } from '../../template/otp-verification';
+import { sendOtpSchema, sendOtpSchemaType } from '../../validation/schema';
+
+// router
 const router = Router();
+
+/**
+ * @route POST /api/email/send-otp
+ * @description Sends an OTP email to the specified email address using a custom email template.
+ * 
+ * @param {sendOtpSchemaType} req.body - The request body containing the email details.
+ * @param {string} req.body.email - The recipient's email address.
+ * @param {string} req.body.name - The recipient's name.
+ * @param {string} req.body.token - The unique token for verification.
+ * @param {string} req.body.otp - The OTP code to be sent to the recipient.
+ * @param {string} req.body.subject - The subject of the email.
+ * 
+ * @returns {Object} 200 - Success response containing a success message.
+ * @returns {Object} 500 - Error response if something goes wrong.
+ * 
+ * @example
+ * // Example request body
+ * {
+ *   "email": "example@domain.com",
+ *   "name": "John Doe",
+ *   "token": "123abc",
+ *   "otp": "123456",
+ *   "subject": "Your OTP Code"
+ * }
+ * 
+ * @example
+ * // Example success response
+ * {
+ *   "success": true,
+ *   "message": "Email OTP sent successfully"
+ * }
+ * 
+ * @example
+ * // Example error response
+ * {
+ *   "success": false,
+ *   "message": "Internal server error!"
+ * }
+ */
 
 router.post(
     '/',
--- src/routes/send-email/send-password.ts
+++ src/routes/send-email/send-password.ts
@@ -1,16 +1,65 @@
 import { Router, Request, Response } from 'express';
+
+// import render from '@react-email/render';
 import { render } from '@react-email/components';
+
 // validation middleware
 import validate from '../../validation';
+
+// utils imports for sending emails
 import sendZeptoEmail from '../../utils/email-trigger';
 
+// schema & types imports for validation
 import {
     sendPasswordSchema,
     ISendPasswordPayload,
 } from '../../validation/schema';
+
+// template imports
 import { PasswordTemplate } from './../../template/send-password';
 
+// router
 const router = Router();
+
+
+/**
+ * @route POST /api/email/send-password
+ * @description Sends a password email to the specified email address using a custom email template.
+ * 
+ * @param {ISendPasswordPayload} req.body - The request body containing the email details.
+ * @param {string} req.body.email - The recipient's email address.
+ * @param {string} req.body.name - The recipient's name.
+ * @param {string} req.body.password - The password to be sent to the recipient.
+ * @param {string} req.body.reset_link - The password reset link.
+ * @param {string} req.body.subject - The subject of the email.
+ * 
+ * @returns {Object} 200 - Success response containing a success message.
+ * @returns {Object} 500 - Error response if something goes wrong.
+ * 
+ * @example
+ * // Example request body
+ * {
+ *   "email": "example@domain.com",
+ *   "name": "John Doe",
+ *   "password": "mySecurePassword",
+ *   "reset_link": "https://example.com/reset-password",
+ *   "subject": "Your Password Details"
+ * }
+ * 
+ * @example
+ * // Example success response
+ * {
+ *   "success": true,
+ *   "message": "Email OTP sent successfully"
+ * }
+ * 
+ * @example
+ * // Example error response
+ * {
+ *   "success": false,
+ *   "message": "Internal server error!"
+ * }
+ */
 
 router.post(
     '/',
